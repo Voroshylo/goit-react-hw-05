831c378730c4d1d4fdebdba2fc045c48f5393a17
Voroshylo <152655013+Voroshylo@users.noreply.github.com>
2024-07-29 09:50:32 +0300

    123

--- src/components/pages/HomePage/HomePage.jsx
+++ src/components/pages/HomePage/HomePage.jsx
@@ -20,7 +20,7 @@ const HomePage = () => {
   }, []);
 
   return (
-    <div>
+    <div className={css.div}>
       <h2 className={css.title}>The most current movies:</h2>
       {error ? <p>{error}</p> : <MoviesList movies={movies} />}
     </div>
